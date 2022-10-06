48883dc0e60c0179cb0194db8b85ee1a08eb8fa9
DenisSaar <115184745+DenisSaar@users.noreply.github.com>
2022-10-06 19:00:56 +0000

    Add files via upload

---
+++ MyApp.java
@@ -0,0 +1,17 @@
+public class MyApp {
+    public static void main(String[] Args) {
+        double a = 50.43649;
+        double b = 30.54092;
+        String i = "Saar, Denis!";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(i);
+
+
+
+
+    }
+
+}
+
+
