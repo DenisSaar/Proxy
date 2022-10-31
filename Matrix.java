2afec36439cb87f6e5a88207d3fd8600a93541f1
DenisSaar <115184745+DenisSaar@users.noreply.github.com>
2022-10-31 19:24:48 +0000

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,40 @@
+public class Matrix {
+
+
+    public static void main(String[] args) {
+        {
+            int n = 10;
+            int m = 10;
+            int[][] a = new int[n][m];
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    a[i][j] = n * i + j;
+                }
+            }
+            System.out.println("Начальная матрица");
+            System.out.println("------");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.printf("%4d", a[i][j]);
+                }
+                System.out.println();
+            }
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j < n; j++) {
+                    int temp = a[i][j];
+                    a[i][j] = a[j][i];
+                    a[j][i] = temp;
+                }
+            }
+            System.out.println();
+            System.out.println("Новая матрица");
+            System.out.println("------");
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    System.out.printf("%3d", a[i][j]);
+                }
+                System.out.println();
+            }
+        }
+    }
+}
